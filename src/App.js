054f9748330f575cd883e67024a18d6cae654ad8
Shrey Asthana <shreyasthana545@gmail.com>
2023-01-25 01:37:31 +0530

    UPDATE: added userInput to View Functionality

--- src/App.js
+++ src/App.js
@@ -3,13 +3,21 @@ import './App.css';
 
 
 function App() {
+  
+  let [ userInput, setUserInput ] = useState("");
+
+  const inputChangeHandler = (event) => {
+    setUserInput(event.target.value);
+  }
+
   return (
-    <div className="App">
-      <h1 className="welcome-msg">
-        Welcome!
-      </h1>
+    <div className='App'>
+      <h1>Inside Out!!</h1>
+      <input onChange={ inputChangeHandler } className="input-area"></input>
+      <div> Hello { userInput }! </div>
     </div>
-  );
+  )
+
 }
 
 export default App;
