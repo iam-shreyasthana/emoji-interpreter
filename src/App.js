a38202b3c01364c804ad8363af3efa48e7644ca3
Shrey Asthana <shreyasthana545@gmail.com>
2023-01-25 04:21:45 +0530

    added all the functionalities before styling

--- src/App.js
+++ src/App.js
@@ -1,20 +1,59 @@
 import React, { useState } from 'react';
 import './App.css';
 
+const emojiDictionary = {
+  "😊": "Smiling",
+  "😳": "disbelief",
+  "😔": "sad",
+  "🥡": "takeout box",
+  "❤️": "love",
+  "😑": "annoyance" 
+};
+
+
+const emojisArray = Object.keys(emojiDictionary);
 
 function App() {
-  
-  let [ userInput, setUserInput ] = useState("");
 
-  const inputChangeHandler = (event) => {
-    setUserInput(event.target.value);
+  let [ userInputEmoji, setUserInputEmoji ] = useState("");  
+  let [ meaningEmoji, setMeaning ] = useState("Meaning will appear here....!");
+  
+  const emojiInputChangeHandler = (event) => {
+    let userInputEmoji = event.target.value;
+    setUserInputEmoji(userInputEmoji);
+
+    if(userInputEmoji in emojiDictionary){
+      setMeaning(emojiDictionary[userInputEmoji]);
+    }
+    else{
+      setMeaning("We do not have this in our DataBase :(")
+    }
+  }
+
+  const emojiClickHandler = (userInputEmoji) => {
+    setMeaning(emojiDictionary[userInputEmoji]);
   }
 
   return (
     <div className='App'>
-      <h1>Inside Out!!</h1>
-      <input onChange={ inputChangeHandler } className="input-area"></input>
-      <div> Hello { userInput }! </div>
+      <h1>Emoji Interpreter</h1>
+
+      <input onChange={ emojiInputChangeHandler } value={userInputEmoji} placeholder="Put an emoji here to show the meaning" 
+        className="input-area"></input>
+      
+      
+
+      <h2> {userInputEmoji} </h2> {}
+      <div> <h2>{ meaningEmoji } </h2> </div>{}
+      {
+        emojisArray.map((emoji) => (
+          <span
+            onClick={() => emojiClickHandler(emoji)} className="emoji-area">
+            {" "}
+            {emoji}{" "}
+          </span>
+        ))
+      }
     </div>
   )
 
